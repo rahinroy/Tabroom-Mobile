2765e9fd5bdb39a20d319915c493fc8292763445
Nihar Annam <32647525+rahinroy@users.noreply.github.com>
2020-06-13 13:15:15 -0500

    fixed bug with nats jduging

--- lib/class/Judges.dart
+++ lib/class/Judges.dart
@@ -16,13 +16,13 @@ class Judges{
   Future init() async {
     dom = await construct(link);
     var roundNamesElements = dom.getElementsByClassName("bigger bordertop martop centeralign");
-    var roundLinksList = dom.getElementsByClassName("blue half centeralign");
+    var roundLinksList = dom.getElementsByClassName("blue centeralign");
     for (var x = 0; x < roundNamesElements.length; x++){
       eventNames.add(roundNamesElements[x].text.trim());
       print(roundLinksList[x].text.trim());
     }
     for (var x = 0; x < roundLinksList.length; x++){
-        if (roundLinksList[x].text.trim() == "Judge List"){
+        if (roundLinksList[x].text.trim() == "Judge List" || roundLinksList[x].text.trim() == "List & Paradigms"){
         eventLinks.add("https://www.tabroom.com" + roundLinksList[x].attributes['href']);
       }
     }
